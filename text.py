564e46853d05e0508f792e4a7b4c2cf1770b713f
edilson gomes de lima <edskateicevida@gmail.com>
2023-11-01 11:36:11 -0300

    add .0008

---
+++ text.py
@@ -0,0 +1,9 @@
+def main():
+    nome = input("Digite o seu nome: ")
+    idade = int(input("Digite a sua idade: "))
+
+    print("Nome:", nome)
+    print("Idade:", idade)
+
+if __name__ == "__main__":
+    main()
